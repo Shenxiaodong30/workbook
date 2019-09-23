71e40c81c0f7db081716d839e547e8c55c732724
Shenxiaodong30 <1131664288@qq.com>
2019-09-24 00:51:48 +0800

    退出登录完成

---
+++ web_back/js/Utils/user.js
@@ -0,0 +1,23 @@
+var user = {
+    /**
+     * 管理员登录
+     * @param {*} userName  用户名
+     * @param {*} password  密码
+     * @param {*} callBack  回调函数
+     */
+    login: function (userName, password, callBack) {
+        $.post('http://localhost:8000/admin/login', {
+            user_name: userName,
+            password: password
+        }, function (res) {
+            // res 本次请求服务区返回的内容
+            callBack(res);
+        });
+    },
+
+    logout: function (callBack) {
+        $.post('http://localhost:8000/admin/logout', function (res) {
+            callBack(res); 
+        });
+    }
+};
