7ff924d352b20e1c036eb17b536585853248f289
Shenxiaodong30 <1131664288@qq.com>
2019-09-24 16:31:07 +0800

    再次提交

---
+++ web_back/js/Utils/config.js
@@ -0,0 +1,7 @@
+var Base_url = 'http://localhost:8000'
+
+var urlIst = {
+    user_login : Base_url + '/admin/login',
+    user_logout :  Base_url + '/admin/logout',
+    user_getInfo : Base_url + '/admin/getuser'
+}
